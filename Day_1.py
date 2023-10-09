6fd7d291c69b37f0864f8ab50f8c6b230a211613
Andrew Doxy <112598919+DrewDoxy@users.noreply.github.com>
2023-10-09 00:29:44 -0400

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,13 @@
+print("======================")
+print("Band Name Generator!")
+print("======================")
+#Create a greeting
+print("Welcome to the Band Name Generator")
+
+#Ask user for the city they grew up in
+part_1 = input("What's the name of the city you grew up in?\n")
+#Ask user for the name of their first pet
+part_2 = input("What was your first pet?\n")
+
+print(f"Your band name could be " + part_1, part_2)
+
