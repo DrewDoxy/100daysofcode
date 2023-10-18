abb9639b606118d5dfb6b9bd82e32069e9e986d7
DrewDoxy <112598919+DrewDoxy@users.noreply.github.com>
2023-10-17 22:37:10 -0400

    Keeping my git skills sharp with an added comment

--- PycharmProjects/100DaysofCode/Day_5.py
+++ PycharmProjects/100DaysofCode/Day_5.py
@@ -1,3 +1,5 @@
+#I didn't add anything new today, but I did work through some of the exercises for Day 5. Refreshing to keep my Git skills up to date.
+
 print("Welcome to the PyPassword Generator!")
 input("How many letters would you link in your password? ")
 
