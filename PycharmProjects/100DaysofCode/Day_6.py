b2d71b7bf1d6bbbb466869df80002b59bbac986a
DrewDoxy <112598919+DrewDoxy@users.noreply.github.com>
2023-10-22 22:11:34 -0400

    Adding Day 6

---
+++ PycharmProjects/100DaysofCode/Day_6.py
@@ -0,0 +1,21 @@
+#This project is based on Reeborg's World
+#The point was to program a robot to get out of a maze on its own
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
