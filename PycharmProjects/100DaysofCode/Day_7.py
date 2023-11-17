3aed7bdd8380fdd1213c85c3edc42841ff8f327e
DrewDoxy <112598919+DrewDoxy@users.noreply.github.com>
2023-11-16 22:00:54 -0500

    Adding the completed code for Day 7.

--- PycharmProjects/100DaysofCode/Day_7.py
+++ PycharmProjects/100DaysofCode/Day_7.py
@@ -1,21 +1,48 @@
-import random
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+import random, hangman_words, hangman_art
 
-print(f"The chosen word is {chosen_word}.")
+print(hangman_art.logo)
+word_list = hangman_words.word_list
+stages = hangman_art.stages
+chosen_word = random.choice(word_list)
+end_of_game = False
+
 display = []
 
-for i in range(len(chosen_word)):
-    display[len(chosen_word)] == "_"
+word_length = len(chosen_word)
 
-print(display)
+for _ in range(word_length):
+    display += "_"
 
-#guess = input("Please guess a letter: ").lower()
+lives = 6
+while not end_of_game:
+    guess = input("Please guess a letter: ").lower()
+    if guess in display:
+        print(f"You've already guessed {guess}.")
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"Incorrect! You guessed '{guess}', and you now have {lives} lives left.")
 
-#for letter in chosen_word:
- #   if letter == guess:
-  #      print("Right")
-   #     lives += 1
-    #else:
-     #   print("Wrong")
-      #  lives += 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(stages[lives])
+
+
+
+
+
+if "_" in display:
+    print("You lost!")
+
+print(f"The chosen word was {chosen_word}.")
