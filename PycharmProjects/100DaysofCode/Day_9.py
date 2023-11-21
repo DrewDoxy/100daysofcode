8c32a51ca932efc6c2d8abed18227a762f341706
DrewDoxy <112598919+DrewDoxy@users.noreply.github.com>
2023-11-20 22:23:24 -0500

    Adding some light work before completing the project.

---
+++ PycharmProjects/100DaysofCode/Day_9.py
@@ -0,0 +1,21 @@
+travel = {
+    "France": {"cities visited": ["Lyon","Paris", "Champagne"], "total visits": 13, "favorite destination": "Lyon",},
+    "Switzerland": {"cities visited": ["Zurich", "Grindelwald", "Apenzell"], "total visits": 3, "favorite destination": "Apenzell",}
+}
+
+travel_log = [
+    {
+        "country": "France",
+        "cities visited": ["Lyon", "Paris", "Champagne"],
+        "total visits": 13,
+        "favorite destination": "Lyon",
+    },
+    {
+        "country": "Switzerland",
+        "cities visited": ["Zurich", "Grindelwald", "Apenzell"],
+        "total visits": 3,
+        "favorite destination": "Apenzell",
+    }
+]
+
+print(travel_log)
