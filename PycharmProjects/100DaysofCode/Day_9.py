106412f472b06eff6177db4597ff26e81dccf287
DrewDoxy <112598919+DrewDoxy@users.noreply.github.com>
2023-11-20 23:28:22 -0500

    Finished Day 9's code.

--- PycharmProjects/100DaysofCode/Day_9.py
+++ PycharmProjects/100DaysofCode/Day_9.py
@@ -1,21 +1,39 @@
-travel = {
-    "France": {"cities visited": ["Lyon","Paris", "Champagne"], "total visits": 13, "favorite destination": "Lyon",},
-    "Switzerland": {"cities visited": ["Zurich", "Grindelwald", "Apenzell"], "total visits": 3, "favorite destination": "Apenzell",}
-}
+import os
+from auction_art import logo
 
-travel_log = [
-    {
-        "country": "France",
-        "cities visited": ["Lyon", "Paris", "Champagne"],
-        "total visits": 13,
-        "favorite destination": "Lyon",
-    },
-    {
-        "country": "Switzerland",
-        "cities visited": ["Zurich", "Grindelwald", "Apenzell"],
-        "total visits": 3,
-        "favorite destination": "Apenzell",
-    }
-]
+def clear():
+    print("\n" * 10)
 
-print(travel_log)
+bids = {}
+
+print(logo)
+
+
+def find_highest_bidder(bidding_record):
+    for bidder in bidding_record:
+        highest_bid = 0
+        winner = ""
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"{winner} has the highest bid and is the winner! They won with a bid of ${highest_bid}")
+
+auction_live = True
+
+while auction_live:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    choice = input("Are there any other bidders? Yes or no: ").lower()
+    if choice == "yes":
+        clear()
+    else:
+        auction_live = False
+        find_highest_bidder(bids)
+
+
+
+
+
+
