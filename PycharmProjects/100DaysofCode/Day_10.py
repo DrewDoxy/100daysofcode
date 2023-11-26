5f5a57d9599ff6d695acd4b3665f4edec6302b32
DrewDoxy <112598919+DrewDoxy@users.noreply.github.com>
2023-11-25 23:24:14 -0500

    Completed Day 10.

--- PycharmProjects/100DaysofCode/Day_10.py
+++ PycharmProjects/100DaysofCode/Day_10.py
@@ -1,37 +1,50 @@
-#This day's project is supposed to be creating a calculator app.
-
-def format_name(f_name, l_name):
-    if f_name == "" or l_name == "":
-        return
-    formatted_f_name = f_name.title()
-    formatted_l_name = l_name.title()
-    return f"Result: {formatted_f_name} {formatted_l_name}"
+from calculator_art import logo
 
 
-print("Your name is: " + format_name(input("What is your first name? "), input("What is your last name? ")))
+def add(n1, n2):
+    return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
 
-def is_leap(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+def exponentiation(n1, n2):
+    return n1 ** n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+    "**": exponentiation,
+}
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number? "))
+
+    for symbol in operations:
+        print(symbol)
+
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation from the line above: ")
+        num2 = float(input("What's the second number? "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+            num1 = answer
         else:
-            return True
-    else:
-        return False
+            should_continue = False
+            calculator()
 
-def days_in_month(year, month):
-    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if month == 2 and is_leap(year):
-        return 29
-    else:
-        return month_days[month - 1]  # python lists starts from 0, so need to subtract one to get it on the same frequency.
-
-year = int(input())  # Enter a year
-month = int(input())  # Enter a month
-days = days_in_month(year, month)
-print(days)
-
+calculator()
